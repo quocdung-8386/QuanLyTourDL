a69e052a802ab945df1f260dbda78011293defaf
Quoc Dung <20232890@eaut.edu.vn>
2025-12-13 19:49:47 +0700

    fixxxx

---
+++ app/src/main/java/com/example/quanlytourdl/model/TourDaySchedule.java
@@ -0,0 +1,9 @@
+package com.example.quanlytourdl.model;
+public class TourDaySchedule {
+    public int dayNumber;
+    public String title = "";
+    public String summary = "";
+
+    public TourDaySchedule() {
+    }
+}
